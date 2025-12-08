9aa41b5ff4120b394cecf5dfc082fb5b0ad676b8
Maria Orlova <m.orlova@haulmont.com>
2025-12-08 12:12:21 +0400

    Display information about the reason for process termination #103
    1. Add a button in the State column for the externally completed instances on ProcessInstanceListView
    2. Open the InstanceTerminateReasonView view by clicking the button from the state column. The opened view shows the reason of instance termination and a data grid with historic incidents

--- src/main/java/io/flowset/control/view/processinstance/column/state/ProcessInstanceStateColumnFragment.java
+++ src/main/java/io/flowset/control/view/processinstance/column/state/ProcessInstanceStateColumnFragment.java
@@ -5,17 +5,26 @@
 
 package io.flowset.control.view.processinstance.column.state;
 
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import io.jmix.flowui.DialogWindows;
 import io.jmix.flowui.fragment.FragmentDescriptor;
 import io.jmix.flowui.fragmentrenderer.FragmentRenderer;
 import io.jmix.flowui.fragmentrenderer.RendererItemContainer;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.view.Subscribe;
 import io.jmix.flowui.view.ViewComponent;
+import io.flowset.control.entity.processinstance.ProcessInstanceData;
+import io.flowset.control.entity.processinstance.ProcessInstanceState;
 import io.flowset.control.entity.processinstance.RuntimeProcessInstanceData;
+import io.flowset.control.view.processinstance.terminatereason.InstanceTerminateReasonView;
 import io.flowset.control.view.util.ComponentHelper;
 import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static io.jmix.flowui.component.UiComponentUtils.getCurrentView;
 
 @FragmentDescriptor("process-instance-state-column-fragment.xml")
 @RendererItemContainer("processInstanceDataDc")
@@ -23,8 +32,12 @@ public class ProcessInstanceStateColumnFragment extends FragmentRenderer<Horizon
 
     @Autowired
     protected ComponentHelper componentHelper;
+    @Autowired
+    protected DialogWindows dialogWindows;
     @ViewComponent
     protected Icon incidentIcon;
+    @ViewComponent
+    protected JmixButton viewTerminateReasonBtn;
 
     @Override
     public void setItem(RuntimeProcessInstanceData item) {
@@ -34,5 +47,25 @@ public class ProcessInstanceStateColumnFragment extends FragmentRenderer<Horizon
         getContent().addComponentAsFirst(processInstanceStateBadge);
 
         incidentIcon.setVisible(BooleanUtils.isTrue(item.getHasIncidents()));
+
+        if (item instanceof ProcessInstanceData processInstanceData) {
+            updateComponentsVisibility(processInstanceData);
+        }
     }
+
+    @Subscribe(id = "viewTerminateReasonBtn", subject = "clickListener")
+    public void onViewTerminateReasonBtnClick(final ClickEvent<JmixButton> event) {
+        dialogWindows.view(getCurrentView(), InstanceTerminateReasonView.class)
+                .withViewConfigurer(instanceTerminateReasonView ->
+                        instanceTerminateReasonView.setProcessInstance((ProcessInstanceData) item))
+                .open();
+    }
+
+    protected void updateComponentsVisibility(ProcessInstanceData processInstanceData) {
+        ProcessInstanceState state = processInstanceData.getState();
+        boolean externallyCompleted = state == ProcessInstanceState.COMPLETED && BooleanUtils.isTrue(processInstanceData.getExternallyTerminated());
+
+        viewTerminateReasonBtn.setVisible(externallyCompleted);
+    }
+
 }
