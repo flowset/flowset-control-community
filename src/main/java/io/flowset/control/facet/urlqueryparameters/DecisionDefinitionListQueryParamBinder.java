a07ab9cb9513940e5aad818dc48513cb9bd84f51
Maria Orlova <m.orlova@haulmont.com>
2026-01-27 14:25:37 +0400

    Update Copyright #115

--- src/main/java/io/flowset/control/facet/urlqueryparameters/DecisionDefinitionListQueryParamBinder.java
+++ src/main/java/io/flowset/control/facet/urlqueryparameters/DecisionDefinitionListQueryParamBinder.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Haulmont 2026. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+
 package io.flowset.control.facet.urlqueryparameters;
 
 import com.google.common.base.Strings;
