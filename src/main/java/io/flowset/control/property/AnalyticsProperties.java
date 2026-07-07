783362b4608eefca6dd1b8b4ad2ff38ab4131170
Nikita Shchienko <shchienko@haulmont.com>
2026-07-07 16:05:38 +0400

    Anonymous statistics #273
    ---------
    
    Co-authored-by: Nikita Shchienko <n.shchienko@haulmont.com>
    (cherry picked from commit 37fe3ab4b64f5686588f0fef072ab9b471c3c889)

---
+++ src/main/java/io/flowset/control/property/AnalyticsProperties.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) Haulmont 2025. All Rights Reserved.
+ * Use is subject to license terms.
+ */
+
+package io.flowset.control.property;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "flowset.control.analytics")
+public class AnalyticsProperties {
+
+    /**
+     * Default state of anonymous analytics collection, applied once when the
+     * {@code AnalyticsSettings} row is first created. Afterwards the state is
+     * controlled by the administrator via the About page and stored in the database.
+     */
+    private final boolean enabled;
+
+    /**
+     * Amplitude API key. Empty by default; when empty, analytics is a no-op (nothing is sent).
+     */
+    private final String key;
+
+    public AnalyticsProperties(@DefaultValue("true") boolean enabled,
+                               @DefaultValue("") String key) {
+        this.enabled = enabled;
+        this.key = key;
+    }
+}
